f3317690542132c6758414beb1cc7d16967d8fc5
Bogdan Strinic <BogdanStrinic003@gmail.com>
2024-02-07 22:43:30 -0700

    Created interface class and added required properties and methods.

---
+++ Part2/Interface1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Interfaces_Abstract_Classes.Part2
+{
+    //Define an interface for animals 
+    public interface IAnimal 
+    {
+        //Adding the properties
+        string Name { get; set; }
+        string Colour { get; set; }
+        double Height { get; set; }
+        int Age { get; set; }
+
+        //Adding the methods
+        void Eat();
+        string Cry();
+    }
+
+}
