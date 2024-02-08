e6c37700025560425cc64e49e7d61dd2b6b0ea7c
Bogdan Strinic <BogdanStrinic003@gmail.com>
2024-02-07 18:12:51 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Interfaces_Abstract_Classes
+{
+    //Abstract class representing an Animal
+    public abstract class Animal
+    {
+        
+    }
+}
